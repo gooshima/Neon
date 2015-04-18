5c0f97ecf7ab4dfb8f38fb1b59c861bc5d455795
GoOshima <goshima5@gmail.com>
2015-04-18 13:42:54 +0900

    init

---
+++ js/app.js
@@ -0,0 +1,63 @@
+window.addEventListener("load", function () {
+    console.log("app loaded...2");
+});
+
+$(document).ready(function () {
+
+    var pageId = $('body').attr("id");
+
+    //タイトル表示
+    if (pageId == 'top') {
+        startTittleAnim();
+        $('#toSyncBtn').click(function () {
+            window.location.href = "list.html";
+        });
+    }
+
+    //TODO デモページ
+    if (pageId == 'demo') {
+        $('#syncStartBtn').click(function(){
+            alert("同期を始めます!!　設定情報を Flash Air に送信する。などなど。");
+            /*
+                途中から参加する場合
+                アプリから同期願いを出す。
+                管理者がパターンファイルと、現在の再生時間を送信する。
+            */
+        });
+    }
+
+});
+
+
+//TODO タイトル表示
+function startTittleAnim() {
+    var targetObj = $('.split');
+    var delaySpeed = 100;
+    var fadeSpeed = 0;
+    var targetTxt = targetObj.html();
+
+    targetObj.css({visibility: 'visible'}).children().addBack().contents().each(function () {
+        var elmThis = $(this);
+        if (this.nodeType == 3) {
+            var $this = $(this);
+            $this.replaceWith($this.text().replace(/(\S)/g, '<span class="textSplitLoad">$&</span>'));
+        }
+    });
+    splitLength = $('.textSplitLoad').length;
+    targetObj.find('.textSplitLoad').each(function (i) {
+        splitThis = $(this);
+
+        splitTxt = splitThis.text();
+
+        splitThis.delay(i * (delaySpeed)).css({
+            display: 'inline-block',
+            opacity: '0'
+        }).animate({opacity: '1'}, fadeSpeed);
+    });
+
+    setTimeout(function () {
+        targetObj.html(targetTxt);
+        $('#mainTitle').fadeOut('slow');
+        $('#mainBox').fadeIn('slow');
+    }, splitLength * delaySpeed + fadeSpeed);
+}
